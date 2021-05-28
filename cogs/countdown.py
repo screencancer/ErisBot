611d88523e29b01db62fb3d4eeb6b9f9e7fa21db
twsmatt2 <twsmatt2@gmail.com>
2021-05-27 21:28:25 -0700

    Basic time logic added for countdown

--- cogs/countdown.py
+++ cogs/countdown.py
@@ -4,6 +4,7 @@ import datetime
 import discord
 
 from discord.ext import commands
+from pytz import common_timezones
 
 
 class Countdown(commands.Cog):
@@ -11,13 +12,14 @@ class Countdown(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def settime(self, ctx, month, day, year, time2):
+    async def settime(self, ctx, month, day, year, time2, timezone):
         m, h = time2.split(':')
         m = int(m)
         h = int(h)
         month = int(month)
         day = int(day)
         year = int(year)
+        timezoneend = pytz.timezone(timezone)
 
         endtime = datetime.datetime(year, month, day, h, m)
         print(endtime)
