d95e1eec238a3be8cd7d02715b7ea4d943ec4fe9
twsmatt2 <twsmatt2@gmail.com>
2021-05-28 00:04:18 -0700

    Timezone support

--- cogs/countdown.py
+++ cogs/countdown.py
@@ -1,10 +1,23 @@
 import time
 import pytz
-import datetime
+import datetime as dt
 import discord
 
-from discord.ext import commands
+from pytz import timezone
 from pytz import common_timezones
+from discord.ext import commands
+
+Tz = {
+    'PST': timezone('America/Los_Angeles'),
+    'CEST': timezone('Europe/Berlin'),
+    'EST': timezone('America/New_York'),
+    'UTC': pytz.utc
+}
+# UTC = pytz.utc
+# CEST = timezone('Europe/Berlin')
+# PST = timezone('America/Los_Angeles')
+# EST = timezone('America/New_York')
+# print(CEST.zone, UTC.zone, PST.zone, EST.zone)
 
 
 class Countdown(commands.Cog):
@@ -12,17 +25,20 @@ class Countdown(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def settime(self, ctx, month, day, year, time2, timezone):
-        m, h = time2.split(':')
+    async def settime(self, ctx, month, day, year, time2, timezone1):
+
+        h, m = time2.split(':')
         m = int(m)
         h = int(h)
         month = int(month)
         day = int(day)
         year = int(year)
-        timezoneend = pytz.timezone(timezone)
 
-        endtime = datetime.datetime(year, month, day, h, m)
-        print(endtime)
+        # timezoneend = pytz.timezone(Tz[timezone1])
+        date = dt.datetime(year, month, day, h, m,)
+        print(date)
+        cur_date = dt.datetime.now(Tz[timezone1])
+        print(cur_date)
 
 
 def setup(client):
