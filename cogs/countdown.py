4f5bda4610aa196c80526ea60ce16a736e17d3cb
twsmatt2 <twsmatt2@gmail.com>
2022-03-11 01:00:41 -0500

    Re-Added cogs

---
+++ cogs/countdown.py
@@ -0,0 +1,140 @@
+import time
+import pytz
+import datetime as dt
+import discord
+import asyncio
+
+from datetime import timedelta
+from pytz import timezone
+from pytz import common_timezones
+from discord.ext import commands
+from getpass import getpass
+from mysql.connector import connect, Error
+
+cId = 0
+cancelled = 0
+
+timers = {}
+timers2 = {}
+timers3 = {}
+Tz = {
+    'PST': timezone('America/Los_Angeles'),
+    'CEST': timezone('Europe/Berlin'),
+    'EST': timezone('America/New_York'),
+    'UTC': pytz.utc
+}
+# UTC = pytz.utc
+# CEST = timezone('Europe/Berlin')
+# PST = timezone('America/Los_Angeles')
+# EST = timezone('America/New_York')
+# print(CEST.zone, UTC.zone, PST.zone, EST.zone)
+
+
+class Countdown(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def countdown(self, ctx, eventname, time1, time2, timespecifier, timezone1):
+        serverID = ctx.guild.id
+        chanID = ctx.channel.id
+        msgID = ctx.message.id
+
+        print(serverID)
+        print(chanID)
+
+        if chanID in timers3 and timers3[chanID]:
+            await ctx.send('A timer has already begun.')
+            return
+
+        timers3[chanID] = True
+
+        # Then you add your timer logic
+        while timers3[chanID]:
+            h, m = time2.split(':')
+            m = int(m)
+            h = int(h)
+
+            if timespecifier == 'PM':
+                if h == 12:
+                    h -= 12
+                h += 12
+                print(h)
+            if timespecifier == 'AM':
+                if h == 12:
+                    h -= 12
+            month, day, year = time1.split('/')
+            month = int(month)
+            day = int(day)
+            year = int(year)
+
+            # datetimeFormat = '%Y-%m-%d %H:%M:%S'
+            # timezoneend = pytz.timezone(Tz[timezone1])
+
+            date = dt.datetime(year, month, day, h, m, )
+            cur_date = dt.datetime.now(Tz[timezone1])
+            cur_date.replace(microsecond=0)
+
+            print(cur_date)
+            print(date)
+
+            aware_date = date.astimezone(Tz[timezone1])
+            aware_date.replace(microsecond=0)
+
+            diff = aware_date - cur_date.replace(microsecond=0)
+            print(diff)
+            days, hours, minutes, seconds = diff.days, diff.seconds // 3600, diff.seconds % 3600 / 60.0, diff.seconds % 60
+            print(seconds)
+            time = await ctx.send(
+                f'{eventname} Countdown \nDays left: {int(days)} \nHours left: {int(hours)} \nMinutes left: {int(minutes)} \n Seconds left: {seconds}')
+            global cancelled
+            cancelled = 0
+
+            while diff.seconds != 0 & cancelled == 0 and timers3[chanID]:
+                if cancelled == 1:
+                    break
+                if seconds <= 0 and minutes <= 0 and hours <= 0 and days <= 0:
+                    break
+                if days < 0:
+                    days = 0
+                    minutes = 0
+                    hours = 0
+                    seconds = 0
+                    await time.edit(
+                        content=f'{eventname} Countdown \nDays left: {int(days)} \nHours left: {int(hours)} \nMinutes left: {int(minutes)} \n Seconds left: {seconds}')
+                    print("Stopping")
+                    break
+
+                await asyncio.sleep(60)
+
+                cur_date = dt.datetime.now(Tz[timezone1])
+                aware_date = date.astimezone(Tz[timezone1])
+
+                diff = aware_date - cur_date.replace(microsecond=0)
+
+                days, hours, minutes, seconds = diff.days, diff.seconds // 3600, diff.seconds % 3600 / 60.0, diff.seconds % 60
+
+                await time.edit(
+                    content=f'{eventname} Countdown \nDays left: {int(days)} \nHours left: {int(hours)} \nMinutes left: {int(minutes)} \n Seconds left: {seconds}')
+
+        # After coming out of the loop, it would be a good idea to delete it from the dictionary
+
+        del timers3[chanID]
+
+    @commands.command()
+    async def stop(self, ctx):
+        serverID = ctx.guild.id
+        chanID = ctx.channel.id
+        msgID = ctx.message.id
+
+        if chanID not in timers3:
+            await ctx.send('There is no active countdown in this channel/server.')
+            return
+
+        #timers[serverID] = False
+        timers3[chanID] = False
+        await ctx.send("Stopping timer")
+
+
+def setup(client):
+    client.add_cog(Countdown(client))
---
+++ cogs/main.py
@@ -0,0 +1,39 @@
+import discord
+import random
+from discord.ext import commands
+
+intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
+
+
+class Main(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=['8ball'])
+    async def _8ball(self, ctx, *, question):
+        responses = ["It is certain.",
+                     "It is decidedly so.",
+                     "Without a doubt.",
+                     "Yes - definitely.",
+                     "You may rely on it.",
+                     "As I see it, yes.",
+                     "Most likely.",
+                     "Outlook good.",
+                     "Yes.",
+                     "Signs point to yes.",
+                     "Reply hazy, try again.",
+                     "Ask again later.",
+                     "Better not tell you now.",
+                     "Cannot predict now.",
+                     "Concentrate and ask again.",
+                     "Don't count on it.",
+                     "My reply is no.",
+                     "My sources say no.",
+                     "Outlook not so good.",
+                     "Very doubtful."]
+        await ctx.send(f'Question: {question} \nAnswer: {random.choice(responses)}')
+
+
+def setup(client):
+    client.add_cog(Main(client))
